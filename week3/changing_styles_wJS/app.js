c08c18d77e273eba2a1e9055c19760bd1bc8c964
Charles Adams <charleskellyadams@gmail.com>
2021-03-05 04:01:39 -0700

    Assignment 2

--- week3/changing_styles_wJS/app.js
+++ week3/changing_styles_wJS/app.js
@@ -1,6 +1,6 @@
 for(let i = 0; i < 5; i++) {
     let header = document.createElement("h1");
-    header.textContent = "Game Design";
+    header.textContent = "Game Design"
     header.style.fontSize = "20px";
     header.style.fontWeight = "lighter";
     header.style.fontFamily = "sans-serif";
